6862cee1424fdcbcaf327feb641acb6c5151526d
wulihua <wulihua0@gmail.com>
2016-03-09 04:58:48 +0800

    update 330

--- leetcode/330/miniPatches.cc
+++ leetcode/330/miniPatches.cc
@@ -4,9 +4,9 @@ class Solution {
 public:
     //now it can express [1,known_sum)
     int minPatches(vector<int>& nums, int n) {
-        int known_sum = 1, patch = 0, i = 0;
+        size_t known_sum = 1, patch = 0, i = 0;
         while (known_sum <= n) {
-            if (known_sum < nums[i]) {
+            if (i >= nums.size() || known_sum < nums[i]) {
                 //add known_sum into the array to maxmize the span.
                 known_sum += known_sum;
                 patch++;
@@ -30,6 +30,8 @@ int main() {
     assert(1 == f({1,3}, 6));
     assert(2 == f({1,5,10}, 22));
     assert(3 == f({1,5,10}, 23));
+    assert(3 == f({}, 7));
+    std::cout << f({1,2,31,43}, 2147483647) << "\n";
 
     return 0;
 }
