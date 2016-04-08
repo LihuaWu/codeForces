d947757edc6b5aefd0b8693f47f698b71f82f99a
wulihua <wulihua0@gmail.com>
2016-04-08 16:53:42 +0800

    add all.

---
+++ codeforce/659/G.cc
@@ -0,0 +1,16 @@
+/*
+ * http://codeforces.com/problemset/problem/659/G
+ * G. Fence Divercity
+ * time limit per test2 seconds
+ * memory limit per test256 megabytes
+ * inputstandard input
+ * outputstandard output
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
