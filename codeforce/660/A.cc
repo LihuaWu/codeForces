f299d3dca8e92015cb9a09088a27a89bfa5e6a84
wulihua <wulihua0@gmail.com>
2016-04-10 00:24:10 +0900

    add all.

---
+++ codeforce/660/A.cc
@@ -0,0 +1,36 @@
+/*http://codeforces.com/problemset/problem/660/A
+ * A. Co-prime Array
+ * time limit per test1 second
+ * memory limit per test256 megabytes
+ * inputstandard input
+ * outputstandard output
+ */
+
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int64_t a[1010];
+int64_t b[2020];
+
+int main() {
+    int n;
+    int m = 0;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        if (i && __gcd(a[i], a[i-1]) != 1) {
+            b[m] = 1;
+            m++;
+        } 
+        b[m] = a[i];
+        m++;
+    }
+    cout << m - n + 1<< "\n";
+    for (int i = 0; i < m; i++) {
+        cout << b[i] << " ";
+    }
+    cout << "\n";
+    return 0;
+}
