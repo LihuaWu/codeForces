9d7758e62f62679e1d1cefc6b9dca4402736c09e
wulihua <wulihua0@gmail.com>
2016-04-13 22:00:41 +0900

    add all.

---
+++ codeforce/653/D.cc
@@ -0,0 +1,16 @@
+/*
+ * http://codeforces.com/problemset/problem/653/D
+ * D. Delivery Bears
+ * time limit per test2 seconds
+ * memory limit per test256 megabytes
+ * inputstandard input
+ * outputstandard output
+ * */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
