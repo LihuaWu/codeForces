112ff864d19246893690765183546a25122af173
LihuaWu <wulihua0@gmail.com>
2016-05-31 17:15:52 +0800

    finish this tonight.

--- codeforce/c350_670/F/F.cpp
+++ codeforce/c350_670/F/F.cpp
@@ -1,7 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
+// TODO : finish it this tonight.
+int main(int argc, char **argv) {
 
-int main(int argc, char **argv)
-{
+    std::string a, b;
+    cin >> a >> b;
+    map<char, int> v;
+    size_t n = a.size(), m = b.size();
+    for (int i = 0; i < n; i++) { v[a]++; }
+
     return EXIT_SUCCESS;
 }
