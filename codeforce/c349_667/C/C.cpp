f9cbc1d3785a03b1df8b5a90df0d52af278591ce
LihuaWu <wulihua0@gmail.com>
2016-06-01 14:15:47 +0800

    add all.

--- codeforce/c349_667/C/C.cpp
+++ codeforce/c349_667/C/C.cpp
@@ -1,6 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+
+
+const int N = 1e4 + 10;
+bool d[N][2];
+set<string> ans;
+
+
+int main(int argc, char **argv) {
+    string s;
+    cin >> s;
+
+    size_t s_size = s.size();
+    memset(d, 0, sizeof d);
+    d[s_size][0] = d[s_size][1] = true;
+
+    for (int i = s_size-2; i >=5; i--) {
+        if (d[i+2][1] || (d[i+2][0] && s.substr(i, 2) != s.substr(i+2, 2))) {
+            d[i][0] = true;
+            ans.insert(s.substr(i, 2));
+        }
+        if (d[i+3][0] || (d[i+3][1] && s.substr(i, 3) != s.substr(i+3, 3))) {
+            d[i][1] = true;
+            ans.insert(s.substr(i, 3));
+        }
+    }
+    cout << ans.size() << "\n";
+    for (auto & i : ans) { cout << i << "\n"; }
     return EXIT_SUCCESS;
 }
