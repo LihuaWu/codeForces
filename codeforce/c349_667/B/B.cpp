f0b9a8e081afa4d5dfcc193ab75936ecb893199c
LihuaWu <wulihua0@gmail.com>
2016-06-01 12:45:34 +0800

    add all.

--- codeforce/c349_667/B/B.cpp
+++ codeforce/c349_667/B/B.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+const int N = 1e5 + 10;
+int l[N];
+int n;
+
+int main(int argc, char **argv) {
+    cin >> n;
+//    cout << "n:" << n << "\n";
+    for (int i = 0; i < n; i++) {
+        cin >> l[i];
+    }
+    sort(l, l+n);
+    int sum = 0;
+    for_each(l, l+n-1, [&sum](int c) { sum += c; });
+    cout << (abs(sum - l[n-1]) + 1);
+
+
     return EXIT_SUCCESS;
 }
