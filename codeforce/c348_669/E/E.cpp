3955ed95960d82532b02fae67eb31a6c92a2375f
LihuaWu <wulihua0@gmail.com>
2016-06-06 10:40:41 +0800

    add all.

--- codeforce/c348_669/E/E.cpp
+++ codeforce/c348_669/E/E.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+
+map<int, pair<int, int>> count;
+int main(int argc, char **argv) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a, t, x;
+        cin >> a >> t >> x;
+        if (a == 1) {
+            if (count.count(x) == 0) {
+                count[x]
+            }
+
+
+        } else if (a == 2) {
+        } else if (a == 3) {
+        }
+    }
     return EXIT_SUCCESS;
 }
