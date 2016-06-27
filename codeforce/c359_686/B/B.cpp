95fb79783a3fa364846d16084e24d67925df3a90
LihuaWu <wulihua0@gmail.com>
2016-06-27 19:53:30 +0800

    add all.

--- codeforce/c359_686/B/B.cpp
+++ codeforce/c359_686/B/B.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    int n;
+    cin >> n;
+    int a[100];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = n-1; i > 0; --i) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] > a[j+1]) {
+                swap(a[j], a[j+1]);
+                cout << (j+1) << " " << (j+2) << "\n";
+            }
+        }
+    }
     return EXIT_SUCCESS;
 }
