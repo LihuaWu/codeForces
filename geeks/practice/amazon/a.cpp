de0fe43b333df59d87cbf163782cd602216ce7da
LihuaWu <wulihua0@gmail.com>
2016-07-08 17:03:00 +0800

    add all.

---
+++ geeks/practice/amazon/a.cpp
@@ -0,0 +1,27 @@
+// https://www.practice.geeksforgeeks.org/problem-page.php?pid=907
+
+//check if given four points form a square.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void job() {
+    int x[4], y[4];
+    int cx(0), cy(0);
+    for (int i = 0; i < 4; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+}
+
+int main() {
+    int t;
+    cin >> t; 
+    for (int i = 0; i < t; i++) {
+        job();
+    }
+
+
+    return 0;
+}
