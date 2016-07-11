aa8a20fd0ad91f27060642d3d9ceb2a9ec17b8ca
LihuaWu <wulihua0@gmail.com>
2016-07-11 15:24:21 +0800

    add all.

--- codeforce/c360_688/B/B.cpp
+++ codeforce/c360_688/B/B.cpp
@@ -1,6 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+    string s;
+    cin >> s;
+    string p(s);
+    reverse(p.begin(), p.end());
+    cout << (s+p) << "\n";
+
     return EXIT_SUCCESS;
 }
