22e9bdb1689f082a23d90bf57f46a02b49a7936c
LihuaWu <wulihua0@gmail.com>
2016-07-11 16:55:12 +0800

    add all.

--- codeforce/c360_688/D/D.cpp
+++ codeforce/c360_688/D/D.cpp
@@ -1,6 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(int argc, char **argv)
-{
+
+const int N = 1000010;
+
+int isP[N];
+
+int cntP[N];
+
+int main(int argc, char **argv) {
+    for (int i = 2; i < N; i++) {
+        if (!isP[i]) {
+            for (int j = i; j < N; j += i) {
+                isP[j] = i;
+            }
+        }
+    }
+    int n, k;
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        int c;
+        cin >> c;
+        while (c > 1) {
+            int p = isP[c];
+            int cnt = 0;
+            while (c % p == 0) {
+                c /= p;
+                cnt++;
+            }
+            cntP[p] = max(cntP[p], cnt);
+        }
+    }
+
+    bool flag = true;
+    while (k > 1) {
+        flag &= (cntP[isP[k]] > 0);
+        cntP[isP[k]]--;
+        k /= isP[k];
+    }
+    cout << (flag ? "Yes" : "No") << "\n";
     return EXIT_SUCCESS;
 }
